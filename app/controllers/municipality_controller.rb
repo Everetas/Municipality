24216f979200711c0aa7706ba2bb3789a2b0586e
Everetas <Everetas@gmail.com>
2019-04-05 19:12:08 +0300

    added reading from json, displayed streets, building numbers and administrators

--- app/controllers/municipality_controller.rb
+++ app/controllers/municipality_controller.rb
@@ -1,7 +1,29 @@
+require 'net/http'
+require 'json'
+
 class MunicipalityController < ApplicationController
   def index
-    @administrator = Administrator.find(1)
-    @street = Street.find(1)
-    @building = Building.find(1)
+    @streets = Street.all
+
+    @uncleanedStreets = Array.new(@streets)
+
+    for i in 1..3 do
+      url = 'https://syntask.herokuapp.com/streets' + i.to_s + '.json'
+      uri = URI(url)
+      response = Net::HTTP.get(uri)
+      @cleanedStreets = JSON.parse(response)['streets']
+
+      for cleanedStreet in @cleanedStreets do
+        @uncleanedStreets.reject!{ |hash| hash[:name] == cleanedStreet }
+      end
+
+      @uncleanedBuildings = Array.new
+      for uncleanedStreet in @uncleanedStreets do
+        buildings = Building.where(street_id: uncleanedStreet.id)
+        for building in buildings do
+          @uncleanedBuildings.push(building)
+        end
+      end
+    end
   end
 end
