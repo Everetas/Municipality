143fb361c354e7cdd9fe93fde7abea39717ce42c
Everetas <Everetas@gmail.com>
2019-04-05 22:39:07 +0300

    added date checking

--- app/controllers/municipality_controller.rb
+++ app/controllers/municipality_controller.rb
@@ -6,24 +6,30 @@ class MunicipalityController < ApplicationController
     @streets = Street.all
 
     @uncleanedStreets = Array.new(@streets)
+    @uncleanedBuildings = Array.new
 
     for i in 1..3 do
       url = 'https://syntask.herokuapp.com/streets' + i.to_s + '.json'
       uri = URI(url)
       response = Net::HTTP.get(uri)
-      @cleanedStreets = JSON.parse(response)['streets']
 
-      for cleanedStreet in @cleanedStreets do
-        @uncleanedStreets.reject!{ |hash| hash[:name] == cleanedStreet }
+      last_update = JSON.parse(response)['last_update'].to_date
+      current_date = DateTime.now.to_date
+
+      if current_date == last_update then
+        @cleanedStreets = JSON.parse(response)['streets']
+        for cleanedStreet in @cleanedStreets do
+          @uncleanedStreets.reject!{ |hash| hash[:name] == cleanedStreet }
+        end
       end
 
-      @uncleanedBuildings = Array.new
       for uncleanedStreet in @uncleanedStreets do
         buildings = Building.where(street_id: uncleanedStreet.id)
         for building in buildings do
           @uncleanedBuildings.push(building)
         end
       end
+
     end
   end
 end
